3a98c0fd7e43c9a90376029fa0b1aafb5b70e3c7
Robbert Kooiman <robbert@kooiman.dev>
2022-10-18 17:05:26 +0200

    Fixed punctuation and capitalization

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,12 +12,12 @@ export default function Home() {
       <Layout>
         <div className="flex items-center w-full justify-center md:justify-start h-full">
           <div className="text-center md:text-left text-gray-700">
-            <h1 className="font-bold  text-4xl pb-4">{"Hy I'm"}</h1>
+            <h1 className="font-bold  text-4xl pb-4">{"Hi, I'm"}</h1>
             <p className=" text-6xl md:text-8xl  ">Innocent Masuki</p>
             <p className="md:text-xl mb-20 pb-8 ">
-              <span className="font-bold">Freelancer</span>,
-              <span className="font-bold">Web</span> and{" "}
-              <span className="font-bold">Mobile</span> developer.
+              <span className="font-bold">freelance </span>
+              <span className="font-bold">web</span> and{" "}
+              <span className="font-bold">mobile</span> developer.
             </p>
             <Button title={"My Works"} link={"/works"} />
           </div>
