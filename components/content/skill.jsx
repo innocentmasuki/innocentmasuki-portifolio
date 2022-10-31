858116a7be95432c68955ce8ab237cdef60a7ba6
Innocent Masuki <innocentmasuki99@gmail.com>
2022-10-31 19:54:56 +0300

    added no referrer

--- components/content/skill.jsx
+++ components/content/skill.jsx
@@ -1,6 +1,6 @@
 export default function Skill({ icon, title, link }) {
   return (
-    <a href={link} target="_blank">
+    <a href={link} target="_blank" rel="noreferrer">
       <div className="flex group justify-start rounded-2xl border-2 border-gray-700 hover:border-red-500 ">
         <div className="text-4xl p-2 text-gray-700 group-hover:text-red-500">
           {icon}
