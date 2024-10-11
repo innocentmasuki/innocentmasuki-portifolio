5988f2432e4cf23f0ffd6dc028aebc94a32ccd49
Innocent Masuki <innocentmasuki99@gmail.com>
2024-10-11 12:20:42 +0200

    change play pause from the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
 import '../styles/globals.css'
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 
 function MyApp({ Component, pageProps }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  useEffect(() => {
-    if (audioRef.current && isPlaying) {
+
+  const handleAudioPlayer = ()=>{
+    if (audioRef.current && !isPlaying) {
       audioRef.current.loop = true;
       audioRef.current.play();
+      setIsPlaying(true)
+    }else if(audioRef.current && isPlaying){
+      audioRef.current.pause();
+      setIsPlaying(true)
     }
-  }, [isPlaying]);
-  return <div className='bg-white-100 relative' onClick={()=>setIsPlaying(true)}>
+  }
+
+
+  return <div className='bg-white-100 relative' onClick={()=>handleAudioPlayer()}>
     <audio ref={audioRef} src="/audio/innocent-resume-2024.wav" />
     <Component {...pageProps} /></div>
 }
